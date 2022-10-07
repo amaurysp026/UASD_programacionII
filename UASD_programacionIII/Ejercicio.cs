96a819036f52d01c91bd5f3e01c91b7a5bafd2a4
Amaurys German Peralta <amaurys026@hotmail.com>
2022-10-06 22:58:06 -0400

    Agregar archivos de proyecto.

---
+++ UASD_programacionIII/Ejercicio.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UASD_programacionIII
+{
+    internal class Ejercicio
+    {
+        public bool continua { get; set; }
+        public string menu { get; set; }
+        public Ejercicio()
+        {
+            continua = true;
+            menu = @"
+
+Escoje una opcion: 
+
+1. Calcula la multolicacion de dos numeros.
+2. Calcula el promedio de dos numeros.
+3. Aceptar 2 catetos y encontra la hipotenusa.
+4. Acepta 1 cateto e hipotenusam, encontrar el cateto faltante.
+5. Acepta la base y altura de un triangulo, encontrar el area.
+6. Acepta, a, b y c. econtrar el valor de x1 y x2 segun la formula de ecuaciones de segundo grado.
+7. Acepta una frase. indica cuantos caracteres contiene la mima.
+8. Acepta una cantidad de dinero, e indica cuantas veces se puede viajar en el metro con el dinero.
+9. Calcula cuantas hamburguesas se pueden hacer segun la cantidad de materias primas.
+10. Calcula la moneda de pesos a dolares.
+11. Acepta un numero y muestra: Mitad, el cuadrado y el doble.
+12. Calcula la cantidad de energia consumida y cuanto debe pagar al mes.
+13. Determinar la edad de una persona por su fecha de nacimiento.
+14. Convertir grados Cercius a Farenheight y a grados Kelvin
+15. Calcula el costo de un viaje en vehiculo segun el precio del combustible.
+
+ Precione X para salir
+";
+        }
+        public int multiplica(int num1, int num2)
+        {//multiplica dos numeros
+            return num1 * num2;
+        }
+        public double promedio(double num1, double num2)
+        {//promedio de dos numeros
+            return (num1 + num2) / 2;
+        }
+        public double hipotenusa(double cat1, double cat2)
+        {//encuentra la hipotenusa recibiendo los catetos
+            return Math.Pow(cat1, 2) + Math.Pow(cat2, 2);
+        }
+        public double catfaltante(double cat, double hipo)
+        {//encuentra un cateto faltante al introducir un cateto y la hipotenusa
+            return Math.Sqrt(Math.Pow(cat, 2) - Math.Pow(hipo, 2));
+        }
+        public double areatrinagulo(double bas, double alt)
+        {//encuentra el area de un retangulo al colocar la base por la altura / 2
+            return (bas * alt) / 2;
+        }
+        public void esegundogrado(double a, double b, double c)
+        {
+            double raizPositiva = (-b + (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c))) / (2 * a);
+            double raizNegativa = (-b - (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c))) / (2 * a);
+
+            if (raizNegativa.ToString() == "NaN" || raizPositiva.ToString() == "NaN")
+            {
+                Console.WriteLine("sin raices reales");
+            }
+            else if (raizPositiva != raizNegativa)
+            {
+                Console.WriteLine("{0:0.00}", raizNegativa);
+                Console.WriteLine("{0:0.00}", raizPositiva);
+            }
+            else
+            {
+                Console.WriteLine("{0:0.00}", raizPositiva);
+            }
+        }
+        public int frase(string frase)
+        {//Devuelve la catidad de letras que tiene la frase
+            return frase.Count();
+        }
+        public int viajemetro(int tikets, int dinero)
+        {//Devuelve la cantidad de viajes en el metro
+            return dinero / tikets;
+        }
+        public int hamburgueza(int pan, double carne, double tocineta)
+        {//para hacer una hamburgueza se necesita 1 pan, 2 carnes, 1/5 de libra de tocineta.
+            //calcule cuantas hanburguezas se puede hacer segun la cantidad de estas materias primas que existen en un almacen.
+            int hamburguez = 0;
+            //El bucle prepara una hamburguesa mientras existan ingredientes suficientes.
+            while (pan > 0 && carne > 1 && tocineta > 0.1)
+            {
+                hamburguez++;
+                pan--;
+                // Console.WriteLine("Pan:"+pan);
+                tocineta -= 0.2;
+                // Console.WriteLine("tocineta "+tocineta);
+                carne -= 2;
+                // Console.WriteLine("carne " + carne);
+            }
+
+            return hamburguez;
+        }
+
+        public double pesoadolar(double pesos, double preciodolar)
+        {
+
+            return pesos * preciodolar;
+        }
+
+        public double[] pro11(double num)
+        {
+            //busca la mitad del numero
+            double mitad = num / 2;
+            //busca el cuadrado
+            double cuadrado = Math.Pow(num, 2);
+            //el doble 
+            double doble = num * 2;
+            double[] numeros = new double[] { mitad, cuadrado, doble };
+            return numeros;
+        }
+
+        public double precioenergia(double kwcosnumidos)
+        {//chequiar montos en factura de luz actual
+            double monto;
+            double cargofijo = 128.59;
+            if (kwcosnumidos < 200)
+            {//calcula el monto si el cusumo es menor a lo indicado
+                monto = kwcosnumidos * 6.05;
+                return monto + cargofijo;
+            }
+            else
+            {//cacula el monto en caso de ser mayor y le suma lo que se consumiria anterior
+                monto = 200 * 6.05;
+                kwcosnumidos -= 200;
+                monto += kwcosnumidos * 8.59;
+                return monto + cargofijo;
+            }
+
+        }
+        public double edad(DateTime fecha)
+        {//optiene la edad introducida por el usuario y optiene los dias trasncurrido con la fecha actual
+
+            double i = (DateTime.Now - fecha).TotalDays;
+            //divide la cantidad de dias en 365 para optener la cantidad de años
+            i /= 365;
+            return i;
+        }
+
+        public double[] convtemperatura(double temcercius)
+        {
+            //calculamos los grados farenhigth
+            double far = (temcercius * 1.8 + 32);
+            //calcilamos los grados kelvin
+            double kel = temcercius + 273.15;
+            double[] convtemperatura = new double[] { far, kel};
+            //retornamos los resultados
+            return convtemperatura;
+
+        }
+        public double autogasolina(double kmpg,double pgasolina,double km)
+        {
+            return Math.Round( km / kmpg * pgasolina);
+        }
+    }
+
+}
---
+++ UASD_programacionIII/Program.cs
@@ -0,0 +1,287 @@
+﻿using UASD_programacionIII;
+
+Ejercicio ejercicios = new Ejercicio();
+
+do
+{
+    Console.WriteLine(ejercicios.menu);
+    string ops = Console.ReadLine();
+    switch (ops)
+    {
+        case "1":
+            {
+                try
+                {
+                    Console.WriteLine("Coloca primer numero a multiplicar:");
+                    int n1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Coloca el segundo numero a multiplicar:");
+                    int n2 = int.Parse(Console.ReadLine());
+                    //llamamos al metodo y pasamos los datos optenidos el cual devolvera el resultado
+                    Console.WriteLine("El resultado es: " + ejercicios.multiplica(n1, n2));
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error al introducir los datos: " + e.Message + " Recuerda solo introducir Numeros");
+                }
+                break;
+            }
+        case "2":
+            {
+                try
+                {
+                    Console.WriteLine("Coloca primer numero para calcular promedio:");
+                    double n1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Coloca el segundo numero para calcular promedio:");
+                    double n2 = double.Parse(Console.ReadLine());
+                    //llamamos al metodo y pasamos los datos optenidos el cual devolvera el resultado
+                    Console.WriteLine("El resultado es: " + ejercicios.promedio(n1, n2));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error al introducir los datos: " + e.Message + " Recuerda solo introducir Numeros");
+
+                }
+                break;
+            }
+        case "3":
+            {
+                try
+                {
+                    Console.WriteLine("Coloca primer cateto:");
+                    double n1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Coloca el segundo cateto:");
+                    double n2 = double.Parse(Console.ReadLine());
+                    //llamamos al metodo y pasamos los datos optenidos el cual devolvera el resultado
+                    Console.WriteLine("El resultado es: " + ejercicios.hipotenusa(n1, n2));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error al introducir los datos: " + e.Message + " Recuerda solo introducir Numeros");
+
+                }
+                break;
+            }
+        case "4":
+            {
+                try
+                {
+                    Console.WriteLine("Coloca un cateto:");
+                    double n1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Coloca la hipotenusa:");
+                    double n2 = double.Parse(Console.ReadLine());
+                    //llamamos al metodo y pasamos los datos optenidos el cual devolvera el resultado
+                    Console.WriteLine("El cateto faltante es: " + ejercicios.catfaltante(n1, n2));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error al introducir los datos: " + e.Message + " Recuerda solo introducir Numeros");
+
+                }
+                break;
+
+            }
+        case "5":
+            {
+                try
+                {
+                    Console.WriteLine("Coloca la base:");
+                    double n1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Coloca la altura:");
+                    double n2 = double.Parse(Console.ReadLine());
+                    //llamamos al metodo y pasamos los datos optenidos el cual devolvera el resultado
+                    Console.WriteLine("El el area es: " + ejercicios.areatrinagulo(n1, n2));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error al introducir los datos: " + e.Message + " Recuerda solo introducir Numeros");
+
+                }
+                break;
+            }
+        case "6":
+            {
+                try
+                {
+                    //  Console.WriteLine("Coloca valor (a):");
+                    //  double a = double.Parse(Console.ReadLine());
+                    //  Console.WriteLine("Coloca valor (b):");
+                    //  double b = double.Parse(Console.ReadLine());
+                    //  Console.WriteLine("Coloca valor (c):");
+                    //  double c = double.Parse(Console.ReadLine());
+                    //llamamos al metodo y pasamos los datos optenidos el cual devolvera el resultado
+                    //Console.WriteLine("El el area es: " + ejercicios.esegundogrado(a,b,c));
+                    ejercicios.esegundogrado(15, 8, 0);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error al introducir los datos: " + e.Message + " Recuerda solo introducir Numeros");
+
+                }
+                break;
+
+            }
+        case "7":
+            {
+                Console.WriteLine("Coloca una frase");
+                Console.WriteLine("La frace tiene " + ejercicios.frase(Console.ReadLine()).ToString() + " letras");
+                break;
+            }
+        case "8":
+            {
+                try
+                {
+                    //Se optiene la cantidad de dinero del usuario
+                    Console.WriteLine("Por favor dijite la cantidad de dinero en pesos disponible para viajar: ");
+                    int dinero = int.Parse(  Console.ReadLine());
+                    int tikets = 35;
+                    Console.WriteLine("Con " + dinero + " pesos dominicanos, puedes viajar " + ejercicios.viajemetro(tikets, dinero).ToString()
+                        + " veses en el metro de Santo Domingo");
+                }
+                catch (Exception e)
+                {
+                    // EN caso de error de entrada de datos
+                    Console.WriteLine("Error: " + e.Message + " Recuerda colocar el monto de dinero correcto");
+                }
+                break;
+            }
+        case "9":
+            {
+                try
+                {
+                    Console.WriteLine("Coloca la cantidad de pan: ");
+                    int pan = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Coloca la cantidad de carne: ");
+                    double carne = double.Parse(Console.ReadLine());
+                    Console.WriteLine("coloca la cantidad en libras de tosineta");
+                    double tos = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Con estos ingredientes se pueden preparar " + ejercicios.hamburgueza(pan,carne,tos)+" hamburgesas") ;
+                    break;
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Error: " + e.Message + " Procura introducir los datos correctos");
+                    break;
+                }
+            }
+        case "10":
+            {
+                try
+                {
+                    Console.WriteLine("Coloca el valor en pesos que deseas convertir a dolares: ");
+                    double pesos =double.Parse( Console.ReadLine());
+                    Console.WriteLine("El monto en dolares es: " + ejercicios.pesoadolar(pesos,0.019));
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message + " recuerda introducir un valor correcto");
+                   
+                }
+                break;
+            }
+        case "11":
+            {
+                try
+                {
+                    //pedir datos
+                    Console.WriteLine("Coloca un numero para devolver los datos de Mitad, Cuadrado y el Doble de dicho numero: ");
+                    double numero = double.Parse(Console.ReadLine());
+                    Console.WriteLine(@"La mitad del numero es: " + ejercicios.pro11(numero)[0] + "\n " +
+                        "El cuadrado del numero es: " + ejercicios.pro11(numero)[1] + "\n " +
+                        "El doble del numero es: " + ejercicios.pro11(numero)[2]);
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Error: " +e.Message + " recuerda colocar un  numero valido");
+                }
+                break;
+            }
+        case "12":
+            {
+                try
+                {
+                    Console.WriteLine("Coloca la cantidad de energia consumida en un periodo de un mes, expresado en Kwh");
+
+                    double kwconsumidos=double.Parse( Console.ReadLine());
+                    Console.WriteLine("El monto a pagar por la energia consumida es: " + ejercicios.precioenergia(kwconsumidos));
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Error: " + e.Message + " recuerda colocar un numero valido");
+                }
+                break;
+
+            }
+        case "13":
+            {
+                try
+                {
+                    Console.WriteLine("Coloca tu fecha de nacimiento, formato(dia/mes/año)");
+                    DateTime fecha = Convert.ToDateTime(Console.ReadLine());
+                    Console.WriteLine("Tu edad es: " + ejercicios.edad(fecha));
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Error: "+e.Message+" Recuerda colocar la fecha en formato correcto");
+                }
+                break;
+            }
+        case "14":
+            {
+                try
+                {
+                    Console.WriteLine("Por favor coloca la temperatura en Cercius a convertir:");
+                    double temp = double.Parse(Console.ReadLine());
+                    Console.WriteLine("La temperatura en grados Farenheight es:" + ejercicios.convtemperatura(temp)[0]);
+                    Console.WriteLine("La temperatura en grados Kelvin es:" + ejercicios.convtemperatura(temp)[1]);
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Error: " + e.Message + " recuerda colocar la temperatura en el formato correcto");
+                }
+                break ;
+            }
+        case "15":
+            {
+                try
+                {
+                    //establece el precio de la gasolina de la semana de 6/10/2022
+                    double preciogasolina = 243.30;
+                    Console.WriteLine("El precio de la gasolina regular actual es: "+preciogasolina);
+                    Console.WriteLine("Coloca la catidad de KM por galon que da el vehiculo de gasolina.");
+                    double kmg = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Que distancia deseas recorrer en KM");
+                    double km= double.Parse(Console.ReadLine());
+                    Console.WriteLine("El costo del viaje es de " + ejercicios.autogasolina( kmg,preciogasolina,km)+" con el precio de la gasolina regular actual") ;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message + " deves colocar los valores de forma correcta");
+                    
+                }
+                break;
+            }
+        case "x":
+            {
+                ejercicios.continua = false;
+                break;
+            }
+
+        default:
+            {
+                Console.WriteLine("la opcion que a dijitado no es correcta por favor elija una opcion valida.\n"); 
+                break;
+
+            }
+            
+    }
+    Console.WriteLine("\n Precione cualquier tecla para continuar....");
+    Console.ReadKey();
+} while (ejercicios.continua);
